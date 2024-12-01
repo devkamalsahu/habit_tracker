6fe969bcfca9928323f4df5ed144293da821f901
devkamalsahu <devkamal3012@gmail.com>
2024-12-01 21:23:52 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import '/pages/home_page.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+void main() async {
+  // initialize hive
+  await Hive.initFlutter();
+
+  // open a box
+  await Hive.openBox("Habit_Database");
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+      theme: ThemeData(
+          colorScheme: const ColorScheme.light(primary: Colors.green)),
+    );
+  }
+}
